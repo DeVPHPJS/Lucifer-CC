6f2a7775d99699e1e1a3cee20e3b29d653759cc1
EViL YT <76827958+DeVPHPJS@users.noreply.github.com>
2023-05-28 18:56:51 +0300

    Update scrape.py

--- mills/plugins/checkers/scrape.py
+++ mills/plugins/checkers/scrape.py
@@ -33,7 +33,7 @@ ccs = []
 
 @bot_cmd(cmd="scr", text_only = True)
 async def _(m):
-    gf = await m.reply("<code>𝖲𝖼𝗋𝖺𝗉𝗉𝗂𝗇𝗀 𝖭𝗈𝗐...🏴‍☠️🔥</code>")
+    #gf = await m.reply("<code>𝖲𝖼𝗋𝖺𝗉𝗉𝗂𝗇𝗀 𝖭𝗈𝗐...🏴‍☠️🔥</code>")
     #inp = m.pattern_match.group(1).strip()
     inp = m.text[len('/scr '):]
     if len(inp) < 1:
